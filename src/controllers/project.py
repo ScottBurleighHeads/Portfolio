adce9c289e207dc60e9f5af636b8cc44282441a1
Scott Malone <scottmcarpentry@gmail.com>
2021-04-10 16:28:56 +1000

    Sorting out automation for projects

--- src/controllers/project.py
+++ src/controllers/project.py
@@ -3,4 +3,23 @@ project = Blueprint("project",__name__)
 
 @project.route('/project')
 def projectIndex():
-    return render_template("projects.html")
+    projectData = {
+        "intro":[
+            "What better way to to show my skills then to show you.","hello"
+            ],
+        "frontend":[
+            "This website was built using html, css and bootsrap.", 
+            ],
+        "backend":["Driving this website is Python and its web application framework Flask."
+            ],
+        "cloud":[
+            "To save cost I have deployed this website using an S3 bucket on AWS." 
+            "Using an S3 bucket dramatically decrease's expense's compared to an EC2."
+        ],
+        "devOps":[
+            "Established a CI/CD pipeling with github actions."
+        ],
+        "database":["This is a static website. There is no database attached or required yet."
+        ],
+        }
+    return render_template("projects.html",projectData=projectData)
