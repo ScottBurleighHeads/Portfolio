abe8ecdc033ffe32ffcb8879fce3d2806e12936e
Scott Malone <scottmcarpentry@gmail.com>
2021-04-18 14:11:12 +1000

    Completed unique carousel

--- src/controllers/project.py
+++ src/controllers/project.py
@@ -4,31 +4,50 @@ project = Blueprint("project",__name__)
 @project.route('/project')
 def projectIndex():
     projectData = {
+        "heading":[
+            "Portfolio","Calorie Counter"
+        ],
         "intro":[
-            "What better way to to show my skills then to show you."
+            "What better way to to show my skills then to show you.",
+            "Calorie Counter is a website that can query search a third party API"
+            "for a certain food item. Once found it can return the calories, protein"
+            "fat and carbs. Once signed in to the website the user has an option to store"
+            "results of the query search onto a database."
             ],
         "frontend":[
-            "This website was built using html, css and bootsrap.", 
+            "This website was built using html, CSS, Jinja2 and bootsrap.", 
+            "The website was built using html, Jinja2 and bootstrap"
             ],
-        "backend":["Driving this website is Python and its web application framework Flask."
+        "backend":[
+            "Driving this website is Python and its web application framework Flask.",
+            "The Calorie Counter website is ran using Python and its web application framework Flask. "
+            "There was an ORM built with SQLalchemy."
             ],
         "cloud":[
             "To save cost I have deployed this website using an S3 bucket on AWS." 
-            "Using an S3 bucket dramatically decrease's expense's compared to an EC2."
+            "Using an S3 bucket dramatically decrease's expense's compared to an EC2.",
+            "I temporarily deployed the website on an EC2 hosted by AWS. I set up a PostgreSQL server"
+            "on a RDS using AWS."
         ],
         "devOps":[
-            "Established a CI/CD pipeling with github actions."
+            "Established a CI/CD pipeling with github actions.",
+            "Basic roles were established for the PostgreSQL database and EC2."
         ],
-        "database":["This is a static website. There is no database attached or required yet."
+        "database":[
+            "This is a static website. There is no database attached or required yet.",
+            "A PostgreSQL server was driving the database."
         ],
         "img1":[
-            'img/image10.jpg'
+            'img/image10.jpg',
+            'img/calorieHome.JPG'
         ],
         "img2":[
-            'img/image11.jpg'
+            'img/image11.jpg',
+            'img/calorieCount1.JPG'
         ],
         "img3":[
-            'img/DreamBig.jpg'
+            'img/DreamBig.jpg',
+            'img/calorieCount2.JPG'
         ]
         }
     print(len(projectData["intro"]))
